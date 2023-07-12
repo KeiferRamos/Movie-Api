ee0fa03aa955404859b2717da1770923b2ae8de3
keifer ramos <keiferramos@keifers-MacBook-Air.local>
2023-07-12 13:56:26 +0800

    fix for

--- src/main.ts
+++ src/main.ts
@@ -13,35 +13,4 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(process.env.PORT || 3000);
 }
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri =
-  'mongodb+srv://keiferramos2001:<password>@keiferdb.brheh4c.mongodb.net/?retryWrites=true&w=majority';
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 });
-    console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!',
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-
-run()
-  .then(() => bootstrap())
-  .catch(console.dir);
+bootstrap();
